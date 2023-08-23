9702c7169412a54439d9d5b6319df0a969490b7a
Arogunb2021 <blessingarogunbo@gmail.com>
2023-08-23 08:35:04 +0100

    pol

---
+++ FTM/chain.js
@@ -0,0 +1,25 @@
+
+export const base = {
+  id: 8453,
+  name: 'Base Mainnet',
+  network: 'Base Mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'ETH',
+    symbol: 'ETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://mainnet.base.org'] },
+    default: { http: ['https://mainnet.base.org'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'basescan', url: 'https://basescan.org' },
+    default: { name: 'basescan', url: 'https://basescan.org' },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 11_907_934,
+    },
+  },
+} 
